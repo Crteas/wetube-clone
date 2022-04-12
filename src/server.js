1561101ed207c6f654119a489c244bbc5bb78ab0
Pru <lms314044s2@gmail.com>
2022-04-12 19:25:17 +0900

    Update server.js
    Request, Response

--- src/server.js
+++ src/server.js
@@ -4,10 +4,27 @@ const PORT = 9000;
 
 const app = express(); //Creates an Express application
 
+//                  (request, response)
+const handleHome = (req, res) => {
+  return res.send("bye");
+};
+
+const handlelogin = (req, res) => {
+  return res
+    .status(200)
+    .send("<a href ='https://expressjs.com/'>Login bro</a>");
+};
+
+const handleLove = (req, res) => {
+  return res.send("Love");
+};
+
 //누가 home (/) 으로 get Request를 보내면 반응하는 callback
 //addEventListener("click",handleCLick)과 느낌은 똑같음
 //app.get(route,callback) get requset를 보내고있음!
-app.get("/", () => console.log("someone try to go home."));
+app.get("/", handleHome);
+app.get("/login", handlelogin);
+app.get("/love", handleLove);
 
 const handleListening = () =>
   console.log(`server listenting on port http://localhost:${PORT} 🚀`);
