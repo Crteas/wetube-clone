49a4cab496173573bfad8f4fecacbc98c19a8136
Pru <lms314044s2@gmail.com>
2022-04-14 22:56:17 +0900

    Update server.js
    learn Middleware, Use use

--- src/server.js
+++ src/server.js
@@ -4,27 +4,25 @@ const PORT = 9000;
 
 const app = express(); //Creates an Express application
 
+//Middleware
+//Middleware가 response 할수도 있음
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
 //                  (request, response)
 const handleHome = (req, res) => {
   return res.send("bye");
 };
 
-const handlelogin = (req, res) => {
-  return res
-    .status(200)
-    .send("<a href ='https://expressjs.com/'>Login bro</a>");
-};
-
-const handleLove = (req, res) => {
-  return res.send("Love");
-};
+//use --> global middleware을 만들어줌
+app.use(logger);
 
 //누가 home (/) 으로 get Request를 보내면 반응하는 callback
 //addEventListener("click",handleCLick)과 느낌은 똑같음
-//app.get(route,callback) get requset를 보내고있음!
+//app.get(route,middleware,callback) get requset를 보내고있음!
 app.get("/", handleHome);
-app.get("/login", handlelogin);
-app.get("/love", handleLove);
 
 const handleListening = () =>
   console.log(`server listenting on port http://localhost:${PORT} 🚀`);
