7bf6fc20169c9fc7fb05401619a0f9054d828a9d
Pru <lms314044s2@gmail.com>
2022-04-15 18:08:13 +0900

    Add Morgan
    and recap

--- src/server.js
+++ src/server.js
@@ -1,31 +1,27 @@
 import express from "express"; //node_modules 에서 express찾고있어요!!
+import morgan from "morgan"; //External Middleware!
 
 const PORT = 9000;
 
 const app = express(); //Creates an Express application
+const logger = morgan("combined"); //morgan!!
 
-//Middleware
-//Middleware가 response 할수도 있음
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const homeController = (req, res) => {
+  res.send("hi");
+};
+const loginController = (req, res) => {
+  res.send("login!");
 };
 
-//                  (request, response)
-const handleHome = (req, res) => {
-  return res.send("bye");
-};
-
-//use --> global middleware을 만들어줌
 app.use(logger);
 
-//누가 home (/) 으로 get Request를 보내면 반응하는 callback
-//addEventListener("click",handleCLick)과 느낌은 똑같음
-//app.get(route,middleware,callback) get requset를 보내고있음!
-app.get("/", handleHome);
+app.get("/", homeController);
+app.get("/login", loginController);
 
-const handleListening = () =>
-  console.log(`server listenting on port http://localhost:${PORT} 🚀`);
+const handleListening = () => {
+  console.log(`Server Start! http://localhost:${PORT} 🚀`);
+};
 
 //서버가 리스닝하게 만들기
+//listen(포트, listen 시작하면 실행될 함수)
 app.listen(PORT, handleListening);
