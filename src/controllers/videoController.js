77585b00dd702accb7c18e292ed04b8e95cf75f2
Pru <lms314044s2@gmail.com>
2022-04-25 16:50:43 +0900

    Partials
    using Include

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,10 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => {
   //req.params는 parameter?
-  return res.send(`Video #${req.params.id}`);
+  return res.render("watch");
 };
 export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit Video!");
+  return res.render("edit");
 };
 export const search = (req, res) => res.send("Search Video!");
 export const upload = (req, res) => res.send("Upload Video");
