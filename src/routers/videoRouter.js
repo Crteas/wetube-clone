bd4c438616bce47e689d6ed4159f752af9936a37
Pru <lms314044s2@gmail.com>
2022-04-30 02:55:46 +0900

    learn POST & urlencoded

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {
   watch,
-  edit,
+  getEdit,
   upload,
   deleteVideo,
+  postEdit,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -11,10 +12,10 @@ const videoRouter = express.Router();
 // /:id(파라미터) ":" -->> 얘를 넣는것이 중요함
 // 이렇게하는 이유는 express한테 이게 변수라는걸 알려주기 위해서임
 // /:id를 위에 두면 upload를 파라미터로 착각함.
-videoRouter.get("/upload", upload);
 //(\\d+)는 정규식! \d+는 뒤에 숫자만 받겠다!
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+/* videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit); */
 
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -4,20 +4,22 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import heroRouter from "./routers/heroRouter";
+import { urlencoded } from "express";
 
 const PORT = 9000;
 
 const app = express(); //Creates an Express application
 const logger = morgan("dev"); //morgan!!
-app.use(logger);
-
-console.log();
 
 //express 설정(set)
 //View engine 설정하기. (express) 뷰 엔진을 퍼그로.
 app.set("view engine", "pug");
 //process.cwd() -> 노드가 시작하는 파일기준
 app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+
+//express.urlencoded post의 form안에있는 value들을 이해할 수 있도록 함.
+app.use(express.urlencoded({ extended: true }));
 
 //누가 "/"로 시작하는 url에 들어가면 express 는 globalRouter에 들어가서 url의 마지막 주소를 찾음.
 app.use("/", globalRouter);
